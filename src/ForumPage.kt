3f4642cb7f1932e95947cb499bbc25f0b0539529
DJ_Kovrik <djkovrik@gmail.com>
2017-08-24 16:28:11 +0300

    Yet another page number parsing fix

--- src/ForumPage.kt
+++ src/ForumPage.kt
@@ -5,7 +5,7 @@ class ForumPage {
   lateinit var forumTitle: String
   @Selector("a[href~=.*/forum\\d+/].title", attr = "href", defValue = "0")
   lateinit var forumId: String
-  @Selector("a[title~=.*: \\d+]:contains(Последняя)", attr = "title", format = "(\\d+)", defValue = "0")
+  @Selector("td[nowrap=nowrap]", format = "(\\d+)", defValue = "0")
   lateinit var totalPages: String
   @Selector("table tr:has(td.row4)")
   lateinit var topics: List<Topic>
@@ -26,7 +26,7 @@ fun main(args: Array<String>) {
 
   createRetrofit()
       .create(YapLoader::class.java)
-      .loadForumPage(forumId = 1, startTopicNumber = 0, sortingMode = "last_post")
+      .loadForumPage(forumId = 1, startTopicNumber = 32280, sortingMode = "last_post")
 
       .subscribe({ forumPage ->
 
