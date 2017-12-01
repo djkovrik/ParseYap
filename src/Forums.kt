3e677942b96a26ead7e1d4aa8cfcc339711ddd1f
DJ_Kovrik <djkovrik@gmail.com>
2017-12-01 20:29:46 +0300

    Tweak forum id parsing

--- src/Forums.kt
+++ src/Forums.kt
@@ -2,7 +2,7 @@ import pl.droidsonroids.jspoon.annotation.Selector
 
 class Forums {
   @Selector("td.row4 > b > a.title") lateinit var titles: List<String>
-  @Selector("td.row4 > b > a.title", attr = "href") lateinit var ids: List<String>
+  @Selector("td.row4 > b > a.title", attr = "href", format = "/forum(\\d+)/") lateinit var ids: List<String>
   @Selector("td.row2[nowrap=nowrap]") lateinit var topics: List<LastTopic>
 }
 
