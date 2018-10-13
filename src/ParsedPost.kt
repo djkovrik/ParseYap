43c6466db58091dbcb4c8e5adab1f197bc9b9cc4
DJ_Kovrik <djkovrik@gmail.com>
2018-10-13 13:49:02 +0300

    Update post parser

--- src/ParsedPost.kt
+++ src/ParsedPost.kt
@@ -24,18 +24,21 @@ class ParsedPost(html: String,
     private const val QUOTE_START_TEXT = "Цитата"
     private const val IFRAME_TAG = "iframe"
     private const val IMG_TAG = "img"
+    private const val IMAGE_LOADER_SELECTOR = "yaplakal.com/html/static/video-loader.gif"
     private const val TD_TAG = "td"
     private const val SRC_ATTR = "src"
-    private const val QUOTE_AUTHOR_MARKER = "@"
+    private const val QUOTE_AUTHOR_MARKER_REGEX = " @ \\d{1,2}\\.\\d{2}.\\d{4}"
     private const val QUOTE_MARKER = "Цитата"
+    private const val POST_EDIT_MARKER = "edit"
+    private const val POST_TAGS_BLOCK_SELECTOR = "div.topic-tags"
 
     private val tagsToSkip =
         setOf("#root", "html", "head", "body", "table", "tbody", "tr", "br", "b", "i", "u")
     private val attrsToSkip = setOf("rating", "clear")
     private val contentWhitelist: Whitelist = Whitelist()
-        .addTags("i", "u", "b", "br", "img", "span")
+        .addTags("i", "u", "b", "br", "img", "a", "li", "ul")
         .addAttributes("img", "src")
-        .addAttributes("span", "style")
+        .addAttributes("a", "href")
   }
 
   init {
@@ -46,7 +49,7 @@ class ParsedPost(html: String,
         .filter { element -> !tagsToSkip.contains(element.tagName()) }
         .filter { element ->
           var notSkip = true
-          attrsToSkip.forEach attrs@ { attribute ->
+          attrsToSkip.forEach attrs@{ attribute ->
             if (element.attributes().toString().contains(attribute)) {
               notSkip = false
               return@attrs
@@ -56,71 +59,71 @@ class ParsedPost(html: String,
         }
         .forEach { element ->
           // Texts
-          if (element.tagName() == POST_TAG &&
-              element.className() == POST_TEXT_CLASS) {
+          if (element.tagName() == POST_TAG && element.className() == POST_TEXT_CLASS) {
             element.select(RATING_SELECTOR).remove()
             element.select(EDITED_TIME_SELECTOR).remove()
             element.select(CLIENT_SELECTOR).remove()
             element.select(WARNING_HEADER_SELECTOR).remove()
             element.select(WARNING_TEXT_SELECTOR).remove()
+            element.select(POST_TAGS_BLOCK_SELECTOR).remove()
             element.select(IMG_TAG).not(EMOTICON_SRC_SELECTOR).remove()
 
-            element.html().cleanExtraTags().trimLinebreakTags().apply {
+            element.html().formatPostHtmlCode().trimLinebreakTags().apply {
               if (this.isNotEmpty())
                 content.add(PostText(text = this))
             }
           }
 
           // Quotes
-          if (element.attributes().toString().contains(QUOTE_SELECTOR)
-              && !element.text().contains(QUOTE_START_TEXT)) {
-            element.html().cleanExtraTags().trimLinebreakTags().apply {
+          if (element.attributes().toString().contains(QUOTE_SELECTOR) && !element.text().contains(
+                  QUOTE_START_TEXT
+              )
+          ) {
+            element.html().formatPostHtmlCode().trimLinebreakTags().apply {
               if (this.isNotEmpty())
                 content.add(PostQuote(text = this))
             }
           }
 
           // Quote authors
-          if (element.text().contains(QUOTE_AUTHOR_MARKER) &&
-              !element.html().contains(Regex("\\R"))) {
+          if (element.text().contains(Regex(QUOTE_AUTHOR_MARKER_REGEX)) && !element.html().contains(Regex("[\\r\\n]+"))) {
             content.add(PostQuoteAuthor(text = element.html()))
           } else if (element.text() == QUOTE_MARKER) {
             content.add(PostQuoteAuthor(text = element.html()))
           }
 
           // Spoilers
-          if (element.tagName() == TD_TAG &&
-              element.attributes().toString().contains(SPOILER_SELECTOR)) {
-            element.html().cleanExtraTags().trimLinebreakTags().apply {
+          if (element.tagName() == TD_TAG && element.attributes().toString().contains(SPOILER_SELECTOR)) {
+            element.html().formatPostHtmlCode().trimLinebreakTags().apply {
               if (this.isNotEmpty())
                 content.add(PostHiddenText(text = this))
             }
           }
 
           // Images
+          @Suppress("ComplexCondition")
           if (element.tagName() == IMG_TAG &&
               element.hasAttr(SRC_ATTR) &&
               !element.attr(SRC_ATTR).contains(EMOTICON_SELECTOR) &&
-              !element.attr(SRC_ATTR).contains(WARNING_IMG_SELECTOR)) {
+              !element.attr(SRC_ATTR).contains(IMAGE_LOADER_SELECTOR) &&
+              !element.attr(SRC_ATTR).contains(WARNING_IMG_SELECTOR)
+          ) {
             images.add(element.attr(SRC_ATTR))
           }
 
           // Videos
-          if (element.tagName() == IFRAME_TAG &&
-              element.hasAttr(SRC_ATTR)) {
+          if (element.tagName() == IFRAME_TAG && element.hasAttr(SRC_ATTR)) {
             videos.add(element.attr(SRC_ATTR))
-            videosRaw.add(element.toString())
+            videosRaw.add(element.toString().replace("&amp;", "&"))
           }
 
           // P.S.
-          if (element.attributes().toString().contains("edit")) {
-            content.add(PostScript(
-                text = element.html()))
+          if (element.attributes().toString().contains(POST_EDIT_MARKER)) {
+            content.add(PostScript(text = element.html()))
           }
 
           // Warnings
-          if (element.tagName() == TD_TAG &&
-              element.className() == POST_TEXT_CLASS) {
+          if (element.tagName() == TD_TAG && element.className() == POST_TEXT_CLASS) {
             content.add(PostWarning(element.html()))
           }
         }
@@ -159,6 +162,26 @@ class ParsedPost(html: String,
   private fun String.trimLinebreakTags(): String {
     return this.removePrefix("<br>").removeSuffix("<br>").trim()
   }
+
+  private fun String.formatPostHtmlCode(): String =
+      Jsoup
+          .clean(this, contentWhitelist)
+          .replace("&nbsp;", " ")
+          .replace("/go/?http", "http")
+          .replace("%21", "!")
+          .replace("%23", "#")
+          .replace("%24", "$")
+          .replace("%25", "%")
+          .replace("%26", "&")
+          .replace("%2B", "+")
+          .replace("%2C", ",")
+          .replace("%2D", "-")
+          .replace("%2E", ".")
+          .replace("%2F", "/")
+          .replace("%3A", ":")
+          .replace("%3B", ";")
+          .replace("%3D", "=")
+          .replace("%3F", "?")
 }
 
 interface Content
--- src/TopicPage.kt
+++ src/TopicPage.kt
@@ -1,82 +1,87 @@
 import pl.droidsonroids.jspoon.annotation.Selector
 
 class TopicPage {
-  @Selector("h1.subpage > a.subtitle", defValue = "Unknown")
+  @Selector(value = "h1.subpage > a.subtitle", defValue = "Unknown")
   lateinit var topicTitle: String
-  @Selector("td.bottommenu > font", defValue = "")
+  @Selector(value = "td.bottommenu > font", defValue = "")
   lateinit var isClosed: String
-  @Selector("input[name~=auth_key]", attr = "outerHtml", format = "value=\"([a-z0-9]+)\"", defValue = "")
+  @Selector(value = "input[name~=auth_key]", attr = "outerHtml", regex = "value=\"([a-z0-9]+)\"", defValue = "")
   lateinit var authKey: String
-  @Selector("div.rating-value", defValue = "")
+  @Selector(value = "div.rating-value", regex = "([-\\d]+)", defValue = "0")
   lateinit var topicRating: String
-  @Selector("div[rel=rating] img[src$=rating-cell-minus.gif]", attr = "src", defValue = "")
+  @Selector(value = "div[rel=rating] img[src$=rating-cell-minus.gif]", attr = "src", defValue = "")
   lateinit var topicRatingPlusAvailable: String
-  @Selector("div[rel=rating] img[src$=rating-cell-plus.gif]", attr = "src", defValue = "")
+  @Selector(value = "div[rel=rating] img[src$=rating-cell-plus.gif]", attr = "src", defValue = "")
   lateinit var topicRatingMinusAvailable: String
-  @Selector("div[rel=rating] img[src$=rating-cell-plus-clicked.gif]", attr = "src", defValue = "")
+  @Selector(value = "div[rel=rating] img[src$=rating-cell-plus-clicked.gif]", attr = "src", defValue = "")
   lateinit var topicRatingPlusClicked: String
-  @Selector("div[rel=rating] img[src$=rating-cell-minus-clicked.gif]", attr = "src", defValue = "")
+  @Selector(value = "div[rel=rating] img[src$=rating-cell-minus-clicked.gif]", attr = "src", defValue = "")
   lateinit var topicRatingMinusClicked: String
   @Selector(
       value = "div[rel=rating] a[onclick~=doRatePost]",
-      format = "(?<=\\d{2}, )(\\d+)((?=, ))",
+      regex = "(?<=\\d{2}, )(\\d+)((?=, ))",
       attr = "outerHtml",
-      defValue = "")
+      defValue = "0"
+  )
   lateinit var topicRatingTargetId: String
-  @Selector("table.row3")
+  @Selector(value = "table.row3")
   lateinit var navigation: TopicNavigationPanel
-  @Selector("table[id~=p_row_\\d+]:has(.normalname)")
+  @Selector(value = "table[id~=p_row_\\d+]:has(.normalname)")
   lateinit var posts: List<TopicPost>
 }
 
 class TopicNavigationPanel {
-  @Selector("td[nowrap=nowrap]:has(a[onclick~=multi_page_jump])", format = "\\[(\\d+)\\]", defValue = "1")
+  @Selector(value = "td[nowrap=nowrap]:has(a[onclick~=multi_page_jump])", regex = "\\[(\\d+)\\]", defValue = "1")
   lateinit var currentPage: String
-  @Selector("td[nowrap=nowrap]:has(a[onclick~=multi_page_jump])", format = "(\\d+)", defValue = "1")
+  @Selector(value = "td[nowrap=nowrap]:has(a[onclick~=multi_page_jump])", regex = "(\\d+)", defValue = "1")
   lateinit var totalPages: String
 }
 
 class TopicPost {
-  @Selector(".normalname", defValue = "Unknown")
+  @Selector(value = ".normalname", defValue = "Unknown")
   lateinit var authorNickname: String
-  @Selector("a[title=Профиль]", attr = "href", defValue = "")
+  @Selector(value = "a[title=Профиль]", attr = "href", defValue = "")
   lateinit var authorProfile: String
-  @Selector("a[title=Профиль] img", attr = "src", defValue = "//www.yaplakal.com/html/static/noavatar.gif")
+  @Selector(value = "a[title=Профиль] img", attr = "src", defValue = "//www.yaplakal.com/html/static/noavatar.gif")
   lateinit var authorAvatar: String
-  @Selector("div[align=left][style=padding-left:5px]", format = "Сообщений: (\\d+)", defValue = "0")
+  @Selector(value = "div[align=left][style=padding-left:5px]", regex = "Сообщений: ([-\\d]+)", defValue = "0")
   lateinit var authorMessagesCount: String
-  @Selector("a.anchor", defValue = "")
+  @Selector(value = "a.anchor", defValue = "")
   lateinit var postDate: String
-  @Selector("span[class~=rank-\\w+]", defValue = "")
+  @Selector(value = "span[class~=rank-\\w+]", regex = "([-\\d]+)", defValue = "0")
   lateinit var postRank: String
-  @Selector("a.post-plus", attr = "innerHtml", defValue = "")
+  @Selector(value = "a.post-plus", attr = "innerHtml", defValue = "")
   lateinit var postRankPlusAvailable: String
-  @Selector("a.post-minus", attr = "innerHtml", defValue = "")
+  @Selector(value = "a.post-minus", attr = "innerHtml", defValue = "")
   lateinit var postRankMinusAvailable: String
-  @Selector("span.post-plus-clicked", attr = "innerHtml", defValue = "")
+  @Selector(value = "span.post-plus-clicked", attr = "innerHtml", defValue = "")
   lateinit var postRankPlusClicked: String
-  @Selector("span.post-minus-clicked", attr = "innerHtml", defValue = "")
+  @Selector(value = "span.post-minus-clicked", attr = "innerHtml", defValue = "")
   lateinit var postRankMinusClicked: String
-  @Selector("td[width*=100%][valign*=top]", attr = "innerHtml", defValue = "")
+  @Selector(value = "td[width*=100%][valign*=top]", attr = "innerHtml", defValue = "")
   lateinit var postContent: String
-  @Selector("a[name~=entry]", attr = "outerHtml", format = "entry(\\d+)", defValue = "0")
+  @Selector(value = "a[name~=entry]", attr = "outerHtml", regex = "entry(\\d+)", defValue = "0")
   lateinit var postId: String
+  @Selector(value = "a:containsOwn(цитировать)", attr = "href", defValue = "")
+  lateinit var hasQuoteButton: String
+  @Selector(value = "a:containsOwn(правка)", attr = "href", defValue = "")
+  lateinit var hasEditButton: String
   @Selector("a.title:containsOwn(#)")
   var tags: List<TopicTag> = emptyList()
 }
 
 class TopicTag {
-  @Selector("a.title:containsOwn(#)")
-  lateinit var tag: String
-  @Selector("a.title:containsOwn(#)", attr = "href")
-  lateinit var tagLink: String
+  @Selector("a.title:containsOwn(#)", defValue = "")
+  lateinit var name: String
+  @Selector("a.title:containsOwn(#)", attr = "href", defValue = "")
+  lateinit var link: String
 }
 
 fun main(args: Array<String>) {
 
   createRetrofit()
       .create(YapLoader::class.java)
-      .loadTopicPage(forumId = 2, topicId = 1788232, startPage = 0)
+      .loadTopicPage(forumId = 28, topicId = 1851991, startPage = 0)
       .subscribe({ topicPage ->
 
         println("Title: ${topicPage.topicTitle}")
@@ -102,8 +107,8 @@ fun main(args: Array<String>) {
 
           if (it.tags.isNotEmpty()) {
             it.tags.forEach {
-              println("Tag: ${it.tag}")
-              println("Tag link: ${it.tagLink}")
+              println("Tag: ${it.name}")
+              println("Tag link: ${it.link}")
             }
           }
 
